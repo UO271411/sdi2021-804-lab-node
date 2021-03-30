28078d52e5c985e071826b7e0f9544f2db33dc72
UO271411 <UO271411@uniovi.es>
2021-03-30 19:52:06 +0200

    SDI–2021-804-sesión7-Ejercicio-complementario1

---
+++ routes/rautores.js
@@ -0,0 +1,50 @@
+module.exports = function(app, swig) {
+    app.get('/autores/agregar', function (req, res) {
+        let respuesta = swig.renderFile('views/autores-agregar.html', {
+
+        });
+        res.send(respuesta);
+    })
+    app.get("/autores", function(req, res) {
+        let autores = [ {
+            "nombre" : "Tony Banks",
+            "grupo" : "Genesis",
+            "rol" : "Teclista",
+        }, {
+            "nombre" : "Stewart Copeland",
+            "grupo" : "The Police",
+            "rol" : "Batería",
+        }, {
+            "nombre" : "Jimi Hendrix",
+            "grupo" : "The Jimi Hendrix Experience",
+            "rol" : "Guitarrista",
+        }, {
+            "nombre" : "James Hetfield",
+            "grupo" : "Metallica",
+            "rol" : "Cantante",
+        }, {
+            "nombre" : "Pete wentz",
+            "grupo" : "Fall Out Boy",
+            "rol" : "Bajista",
+        }]
+        let respuesta = swig.renderFile('views/autores.html', {
+            autores : autores
+        });
+        res.send(respuesta);
+    });
+    app.post("/autor", function(req, res) {
+        let nombre = "Autor agregado: "+req.body.nombre;
+        let grupo = " grupo: " +req.body.grupo;
+        let rol = " rol: "+req.body.rol;
+        if(nombre=="Autor agregado: ")
+            nombre = "Nombre no enviado en la petición.";
+        if(grupo==" grupo: ")
+            grupo = "Grupo no enviado en la petición.";
+        if(rol==" rol: ")
+            rol = "Rol no enviado en la petición.";
+        res.send( nombre + "<br>" + grupo + "<br>" + rol);
+    });
+    app.get('/autores/*', function (req, res) {
+        res.redirect("/autores");
+    })
+};
