df5fda353570ebf5c3ec4e0d084a16304f96ba1b
UO271411 <UO271411@uniovi.es>
2021-03-30 20:57:28 +0200

    SDI–2021-804-sesión7-Ejercicio-complementario3

--- routes/rautores.js
+++ routes/rautores.js
@@ -32,6 +32,34 @@ module.exports = function(app, swig) {
         });
         res.send(respuesta);
     });
+    app.get("/autores/filtrar/:rol", function(req, res) {
+        var rol = req.params.rol;
+        let autores = [ {
+            "nombre" : "Tony Banks",
+            "grupo" : "Genesis",
+            "rol" : "Teclista",
+        }, {
+            "nombre" : "Stewart Copeland",
+            "grupo" : "The Police",
+            "rol" : "Batería",
+        }, {
+            "nombre" : "Jimi Hendrix",
+            "grupo" : "The Jimi Hendrix Experience",
+            "rol" : "Guitarrista",
+        }, {
+            "nombre" : "James Hetfield",
+            "grupo" : "Metallica",
+            "rol" : "Cantante",
+        }, {
+            "nombre" : "Pete wentz",
+            "grupo" : "Fall Out Boy",
+            "rol" : "Bajista",
+        }]
+        let respuesta = swig.renderFile('views/autores.html', {
+            autores : autores.filter((author=>author.rol.includes(rol)))
+        });
+        res.send(respuesta);
+    });
     app.post("/autor", function(req, res) {
         let nombre = "Autor agregado: "+req.body.nombre;
         let grupo = " grupo: " +req.body.grupo;
