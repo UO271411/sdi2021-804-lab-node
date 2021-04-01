cd8159fa3584d6b0c709ca337d972e037a112665
UO271411 <UO271411@uniovi.es>
2021-04-01 22:14:21 +0200

    SDI-2021-804-8.2-Arquitectura para el acceso a datos.

--- app.js
+++ app.js
@@ -2,11 +2,14 @@
 let express = require('express');
 let app = express();
 
+let mongo = require('mongodb');
 let swig = require('swig');
 let bodyParser = require('body-parser');
-let mongo = require('mongodb');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+let gestorBD = require("./modules/gestorBD.js");
+gestorBD.init(app,mongo);
 
 app.use(express.static('public'));
 
@@ -18,8 +21,8 @@ app.set('db','mongodb://admin:sdi@tiendamusica-shard-00-00.rw0xe.mongodb.net:270
     'myFirstDatabase?ssl=true&replicaSet=atlas-6v2u4v-shard-0&authSource=admin&retryWrites=true&w=majority');
 
 //Rutas/controladores por lógica
-require("./routes/rusuarios.js")(app, swig); // (app, param1, param2, etc.)
-require("./routes/rcanciones.js")(app, swig, mongo); // (app, param1, param2, etc.)
+require("./routes/rusuarios.js")(app, swig, gestorBD); // (app, param1, param2, etc.)
+require("./routes/rcanciones.js")(app, swig, gestorBD); // (app, param1, param2, etc.)
 require("./routes/rautores.js")(app, swig); // (app, param1, param2, etc.)
 
 
