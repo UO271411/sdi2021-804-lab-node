e96f9f2279cc9126529532d3d59a6f6748faca3b
UO271411 <UO271411@uniovi.es>
2021-04-12 13:16:11 +0200

    SDI-2021-804-9.2-Redirección de peticiones.

--- app.js
+++ app.js
@@ -93,6 +93,9 @@ require("./routes/rcanciones.js")(app, swig, gestorBD); // (app, param1, param2,
 require("./routes/rautores.js")(app, swig); // (app, param1, param2, etc.)
 require("./routes/rcomentarios.js")(app, swig, gestorBD); // (app, param1, param2, etc.)
 
+app.get('/', function (req, res) {
+    res.redirect('/tienda');
+})
 
 // lanzar el servidor
 app.listen(app.get('port'), function(){
