d2d522db49849a050426c33e879fc235b44853af
UO271411 <UO271411@uniovi.es>
2021-04-20 02:54:39 +0200

    SDI-2021-804-10.3-SW Cliente JQuery + AJAX.

--- routes/rapicanciones.js
+++ routes/rapicanciones.js
@@ -82,6 +82,23 @@ module.exports = function(app, gestorBD) {
         });
     });
 
+    app.get("/api/cancion/:id", function(req, res) {
+        let criterio = { "_id" : gestorBD.mongo.ObjectID(req.params.id)}
+
+        gestorBD.obtenerCanciones(criterio,function(canciones){
+            if ( canciones == null ){
+                res.status(500);
+                res.json({
+                    error : "se ha producido un error"
+                })
+            } else {
+                res.status(200);
+                res.send( JSON.stringify(canciones[0]) );
+            }
+        });
+    });
+
+
     app.post("/api/autenticar/", function(req, res) {
         let seguro = app.get("crypto").createHmac('sha256', app.get('clave'))
             .update(req.body.password).digest('hex');
