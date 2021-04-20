19bec1fe8096344fff54d9083aec6fbb8ee3d528
UO271411 <UO271411@uniovi.es>
2021-04-20 03:50:27 +0200

    SDI-2021-804-10.5-SW Consumir SW desde NodeJS.

--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -27,6 +27,54 @@ module.exports = function(app, swig, gestorBD) {
                     });
                 res.send(respuesta);
             } else {
+                let configuracion = {
+                    url: "https://www.freeforexapi.com/api/live?pairs=EURUSD",
+                    method: "get",
+                    headers: {
+                        "token": "ejemplo",
+                    }
+                }
+                let rest = app.get("rest");
+                rest(configuracion, function (error, response, body) {
+                    console.log("cod: " + response.statusCode + " Cuerpo :" + body);
+                    let objetoRespuesta = JSON.parse(body);
+                    let cambioUSD = objetoRespuesta.rates.EURUSD.rate;
+                    // nuevo campo "usd"
+                    canciones[0].usd = cambioUSD * canciones[0].precio;
+                    criterio = { "cancion_id" :  gestorBD.mongo.ObjectID(req.params.id) };
+                    gestorBD.obtenerComentarios(criterio,function(comentarios){
+                        let respuesta;
+                        criterio = {"usuario": req.session.usuario};
+
+                        gestorBD.obtenerCompras(criterio, function (compras) {
+                            if (compras == null) {
+                                let respuesta = swig.renderFile('views/error.html',
+                                    {
+                                        texto : "Error al listar."
+                                    });
+                                res.send(respuesta);
+                            } else {
+                                if(propietario(req, canciones, compras)) {
+                                    respuesta = swig.renderFile('views/bcancion.html',
+                                        {
+                                            cancion: canciones[0],
+                                            comentarios: comentarios,
+                                            propietario: true
+                                        });
+                                } else {
+                                    respuesta = swig.renderFile('views/bcancion.html',
+                                        {
+                                            cancion: canciones[0],
+                                            comentarios: comentarios,
+                                            propietario: false
+                                        });
+                                }
+                                res.send(respuesta);
+                            }
+                        });
+                    });
+                })
+                /*
                 criterio = { "cancion_id" :  gestorBD.mongo.ObjectID(req.params.id) };
                 gestorBD.obtenerComentarios(criterio,function(comentarios){
                     let respuesta;
@@ -59,6 +107,7 @@ module.exports = function(app, swig, gestorBD) {
                         }
                     });
                 });
+                */
             }
         });
     });
